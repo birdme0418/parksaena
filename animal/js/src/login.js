046cd497f3e3583a1741011f4e82ea89e159c5c8
parksaena <birdme0418@gmail.com>
2017-10-26 13:52:34 +0900

    파일위치 수정

--- animal/js/src/login.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-  var wrap = $('#wrap');
-  wrap.prepend('<section id="loginBox"></section>');
-  var loginBox = wrap.children('#loginBox');
-
-  loginBox.load('./login_01.html');
-})(this.jQuery);
